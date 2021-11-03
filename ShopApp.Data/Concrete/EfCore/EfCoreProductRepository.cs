0c6e3f30467ecf4870b816d00f7edcdf36e94817
Pərviz Hacili <hacili389@gmail.com>
2021-11-04 02:09:11 +0400

    Add ShopApp.Data Class Library

---
+++ ShopApp.Data/Concrete/EfCore/EfCoreProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using ShopApp.Data.Abstract;
+using ShopApp.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopApp.Data.Concrete.EfCore
+{
+    public class EfCoreProductRepository : IProductRepository
+    {
+        private ShopContext db = new ShopContext();
+        public void Create(Product entity)
+        {
+            db.Products.Add(entity);
+            db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetPopularProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
