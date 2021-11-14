ff38d51f4b39873dcb899e186f0f022c6285cbe1
Pərviz Hacili <hacili389@gmail.com>
2021-11-15 02:11:23 +0400

    add CreateProduct Page

--- ShopApp.WebUI/Controllers/AdminController.cs
+++ ShopApp.WebUI/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ShopApp.Business.Abstract;
+using ShopApp.Entity;
 using ShopApp.WebUI.Models;
 using System;
 using System.Collections.Generic;
@@ -21,6 +22,27 @@ namespace ShopApp.WebUI.Controllers
             {
                 Products=_productService.GetAll()
             });
+        }
+
+        [HttpGet]
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(ProductModel productModel)
+        {
+            var entity = new Product()
+            {
+                Name = productModel.Name,
+                Url = productModel.Url,
+                Price=productModel.Price,
+                Description=productModel.Description,
+                ImageUrl=productModel.ImageUrl
+            };
+            _productService.Create(entity);
+            return RedirectToAction("ProductList");
         }
     }
 }
--- ShopApp.WebUI/Models/ProductModel.cs
+++ ShopApp.WebUI/Models/ProductModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,13 @@ namespace ShopApp.WebUI.Models
     public class ProductModel
     {
         public int ProductId { get; set; }
+        [Display(Name="Name",Prompt ="Enter Product Name")]
         public string Name { get; set; }
-        public double? Price { get; set; }
+        public string Url { get; set; }
+        public decimal? Price { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
         public bool IsApproved { get; set; }
+        public bool IsHome { get; set; }
     }
 }
