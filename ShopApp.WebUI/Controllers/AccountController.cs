6b0117d273d52c40981e692062b907854bf85a87
Pərviz Hacili <hacili389@gmail.com>
2021-11-22 21:50:50 +0400

    Create Registerpage

--- ShopApp.WebUI/Controllers/AccountController.cs
+++ ShopApp.WebUI/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ShopApp.WebUI.Identity;
+using ShopApp.WebUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,48 @@ namespace ShopApp.WebUI.Controllers
 {
     public class AccountController : Controller
     {
+        private UserManager<User> _userManager;
+        private SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager,SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task <IActionResult> Register(RegisterModel registerModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(registerModel);
+            }
+
+            var user = new User()
+            {
+                FirstName = registerModel.FirstName,
+                LastName=registerModel.LastName,
+                UserName=registerModel.UserName,
+                Email=registerModel.Email
+            };
+            var result = await _userManager.CreateAsync(user, registerModel.Password);
+            if(result.Succeeded)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            ModelState.AddModelError("Password", "Bilinməyən xəta baş verdi zəhmət olmasa yenidən cəhd edin");
+            return View(registerModel);
         }
     }
 }
