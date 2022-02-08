02c3b2fd42d7406021d15ed622aae5f78e236489
ParvizHacili <parviz.hajili@gmail.com>
2022-02-08 19:35:40 +0400

    Simple change

--- ShopApp.WebUI/Helpers/UiMessages.cs
+++ ShopApp.WebUI/Helpers/UiMessages.cs
@@ -8,7 +8,7 @@ namespace ShopApp.WebUI.Helpers
     public class UiMessages
     {
         public const string RequiredMessage = " mütləq daxil edilməlidir!";
-        public const string StringLengthMessage = "simvoldan artıq ola bilməz";
+        public const string StringLengthMessage = " simvoldan artıq ola bilməz";
         public const string CreateSuccesMessage = "Uğurla əlavə olundu!";
         public const string UpdateSuccesMessage = "Uğurla yeniləndi!";
     }
